8061adcb435337b84146331b9d9bc68234a2869c
Serhii Panferov <serhii.panferov@gmail.com>
2025-11-20 20:59:05 +0100

    feat: Add logs clear command

---
+++ app/Console/Commands/Logs/ClearLogFile.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands\Logs;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Process;
+
+class ClearLogFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // TODO update to use Storage facade
+        // Clear the log files and select what to clear
+        $result = Process::run('echo "" > storage/logs/laravel.log && echo "" > storage/logs/cron.log');
+        if ($result->successful()) {
+            echo $result->output();
+        } else {
+            echo $result->errorOutput();
+        }
+    }
+}
